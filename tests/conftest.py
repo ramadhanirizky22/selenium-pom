b892c9652e1013a638fba66bd158e5005b2fba76
ramadhanirizky22 <rizkypenpenz@gmail.com>
2026-08-15 12:13:25 +0700

    chore: configure pythonpath for local execution and fix gitignore and test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
-import pytest
-from utils.webdriver_factory import get_driver
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+import pytest
+from utils.webdriver_factory import get_driver
 
 @pytest.fixture(scope="function")
 def driver():
